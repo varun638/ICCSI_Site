847fbdc10a060cb496360ef602fda9b4193365e8
varun638 <varunsubash5@gmail.com>
2025-02-12 23:20:59 +0530

    5th change

--- Backend/index.js
+++ Backend/index.js
@@ -30,6 +30,9 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+app.get('/'),async (req, res) => {
+return res.status(200).json(' successfully' );
+}
 app.post('/api/contact', async (req, res) => {
   const { user_name, user_email, user_location, message } = req.body;
 
